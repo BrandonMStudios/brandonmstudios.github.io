8767513cce292d50c56ca689c39e3e22eb36b08b
Brandon Martin <brandonmstudios@gmail.com>
2016-04-18 21:25:51 -0500

    Testing Depictions Now

---
+++ Easy-Repo-Depictions-master/template/index/devLink.php
@@ -0,0 +1,4 @@
+<ul>
+    <li>
+        <a class=\"devLink\" target=\"_blank\" href="hhttps://twitter.com/brandonmstudios">@ADeveloper on Twitter</a></li>
+    <li>
---
+++ Easy-Repo-Depictions-master/template/index/footer.php
@@ -0,0 +1,3 @@
+<p><a href="http://rcrepo.com" target="_blank">Brandon Martin's Personal Repo</a></p>
+</body>
+</html>
