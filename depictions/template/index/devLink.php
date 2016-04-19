cd493a2966ca859700ee71ad5a40bd0633963cb4
Brandon Martin <brandonmstudios@gmail.com>
2016-04-18 21:53:24 -0500

    New Depictions

--- depictions/template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li>
-        <a class=\"devLink\" target=\"_blank\" href="hhttps://twitter.com/brandonmstudios">@ADeveloper on Twitter</a></li>
-    <li>
--- depictions/template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="http://rcrepo.com" target="_blank">Brandon Martin's Personal Repo</a></p>
-</body>
-</html>
